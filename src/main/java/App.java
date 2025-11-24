a2ac56c746f35c5c1ca558bd4297bfa1b8261b15
pxrvx06 <pabharadiya@gmail.com>
2025-11-24 09:32:54 +0530

    Test auto build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
-public class App{
-    public static void main(String[] args){
-        System.out.println("Hello from Jenkins Build!");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Automated Jenkins Build!");
+        System.out.println("This build was triggered automatically!");
     }
 }
